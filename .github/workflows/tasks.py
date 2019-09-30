aa546bf28d81424845172ef2efe744a6a4e3062d
FriendsOfGalaxy <FriendsOfGalaxy@gmail.com>
2019-09-30 11:59:55 +0200

    fix tasks

--- .github/workflows/tasks.py
+++ .github/workflows/tasks.py
@@ -39,13 +39,12 @@ def release():
 
     # parent dir in case SRC is curr dir
     output = os.path.join('..', 'build')
-    print('preparing deploy files')
-    asset_paths = config.deploy('../build')
+    asset_paths = config.deploy(output)
 
-    _run('hub',
-        f'release create {version_tag}'
-        f'-m {RELEASE_TITLE.format(version_tag)}\n{RELEASE_DESC.format(version_tag)}'
-        f'-a {" -a ".join(asset_paths)}'
+    _run('hub', 'release', 'create', version_tag,
+        '-m', RELEASE_TITLE.format(version_tag),
+        '-m', RELEASE_DESC.format(version_tag),
+        '-a', " -a ".join(asset_paths)
     )
 
     assets = []
@@ -65,10 +64,10 @@ def release():
     with open(RELEASE_FILE, 'w') as f:
         json.dump(data, f, indent=4)
 
-    _run(f'git status')
-    _run(f'git remote set-url origin https://{FOG}:{token}@github.com/{user_repo_name}.git')
-    _run(f'git add {RELEASE_FILE}')
-    _run(f'git commit -m {RELEASE_FILE_COMMIT_MESSAGE}')
+    _run('git', 'status')
+    _run('git', 'remote', 'set-url', 'origin', f'https://{FOG}:{token}@github.com/{user_repo_name}.git')
+    _run('git', 'add', RELEASE_FILE)
+    _run('git', 'commit', '-m', RELEASE_FILE_COMMIT_MESSAGE)
 
 
 if __name__ == "__main__":
