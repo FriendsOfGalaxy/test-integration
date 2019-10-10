4639547b21055a2a249e774a3968c5b7d6a73bf9
FriendsOfGalaxy <FriendsOfGalaxy@gmail.com>
2019-10-10 13:22:43 +0200

    fix tests on manual trigger

--- .github/workflows/tests.py
+++ .github/workflows/tests.py
@@ -14,9 +14,8 @@ def test_manifest():
     with open(manifest_location, 'r') as f:
         manifest = json.load(f)
 
-    base_ref = os.environ['BASE_REF']
     prev_manifest  = '__prev_manifest.json'
-    subprocess.run(f"git show origin/{base_ref}:{manifest_location} > {prev_manifest}", shell=True)
+    subprocess.run(f"git show origin/master:{manifest_location} > {prev_manifest}", shell=True)
     with open(prev_manifest, 'r') as f:
         prev_manifest = json.load(f)
 
